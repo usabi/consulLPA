6d83617eff9f1a37ef580ad1edf9235470679e0e
Ignacio Aliende García <ialiendeg@gmail.com>
2017-11-22 13:50:12 +0000

    Synchronizes schema with database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115164152) do
+ActiveRecord::Schema.define(version: 20171122094603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 20171115164152) do
   create_table "budget_headings", force: :cascade do |t|
     t.integer "group_id"
     t.string  "name",       limit: 50
-    t.integer "price",      limit: 8
+    t.float   "price"
     t.integer "population"
     t.string  "slug"
   end
