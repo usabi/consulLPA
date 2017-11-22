d6d65a41740f343c67ad191c5a96b2d903c381c5
Ignacio Aliende García <ialiendeg@gmail.com>
2017-11-22 12:01:37 +0000

    Configures email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
-  config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.asset_host = "http://#{Rails.application.secrets.server_name}"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              Rails.application.secrets.smtp_server,
+    port:                 Rails.application.secrets.port,
+    domain:               Rails.application.secrets.domain,
+    user_name:            Rails.application.secrets.username,
+    password:             Rails.application.secrets.password,
+    authentication:       'plain',
+    #tls:                  true,
+    openssl_verify_mode:  OpenSSL::SSL::VERIFY_NONE,
+    enable_starttls_auto: true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
