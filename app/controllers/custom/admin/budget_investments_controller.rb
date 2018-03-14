bac0d888930e4d96b1339927c713c15766f27db4
Ignacio Aliende García <ialiendeg@gmail.com>
2018-03-14 09:41:14 +0000

    Adds location and organization_name in admin budget investments edition

---
+++ app/controllers/custom/admin/budget_investments_controller.rb
@@ -0,0 +1,9 @@
+require_dependency Rails.root.join('app', 'controllers', 'admin', 'budget_investments_controller').to_s
+class Admin::BudgetInvestmentsController
+  def budget_investment_params
+    params.require(:budget_investment)
+          .permit(:title, :description, :external_url, :heading_id, :administrator_id,
+                  :tag_list, :valuation_tag_list, :incompatible, :organization_name, :location,
+                  :selected, valuator_ids: [])
+  end
+end
