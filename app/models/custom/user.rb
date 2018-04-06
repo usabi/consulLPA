7eb2080e9cceb4ddedcb75affc0aed0fc6ff065a
Ignacio Aliende García <ialiendeg@gmail.com>
2018-04-06 11:38:11 +0100

    Fixes exception when user doesn't have date of birth

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,6 +1,7 @@
 require_dependency Rails.root.join('app', 'models', 'user').to_s
 class User
   def minimum_votation_required_age?
+    return false unless date_of_birth.present?
     Time.current - date_of_birth > 18.years
   end
 end
