2c033ffd52dfa44a921c4b5e7ebceabfdd886215
Nauzet Melián Batista <nmelian@usabi.es>
2019-09-24 15:26:22 +0100

    ejecutadas las nuevas migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -187,7 +187,8 @@ ActiveRecord::Schema.define(version: 20190205131722) do
 
   create_table "budget_headings", force: :cascade do |t|
     t.integer "group_id"
-
+    t.string  "name",                 limit: 50
+    t.float   "price"
     t.integer "population"
     t.string  "slug"
     t.boolean "allow_custom_content",            default: false
@@ -266,11 +267,11 @@ ActiveRecord::Schema.define(version: 20190205131722) do
     t.boolean  "winner",                                                               default: false
     t.boolean  "incompatible",                                                         default: false
     t.integer  "community_id"
-    t.boolean  "visible_to_valuators",                        default: false
-    t.integer  "valuator_group_assignments_count",            default: 0
+    t.boolean  "visible_to_valuators",                                                 default: false
+    t.integer  "valuator_group_assignments_count",                                     default: 0
     t.datetime "confirmed_hide_at"
     t.datetime "ignored_flag_at"
-    t.integer  "flags_count",                                 default: 0
+    t.integer  "flags_count",                                                          default: 0
   end
 
   add_index "budget_investments", ["administrator_id"], name: "index_budget_investments_on_administrator_id", using: :btree
