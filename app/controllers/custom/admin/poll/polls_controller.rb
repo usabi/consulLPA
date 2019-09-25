e6d592aea045beb15fd8a73069a25ce7f55e8e2c
Nauzet Melián Batista <nmelian@usabi.es>
2019-09-25 11:58:09 +0100

    Ya he cambiado los customs de app/controllers/custom/

--- app/controllers/custom/admin/poll/polls_controller.rb
+++ app/controllers/custom/admin/poll/polls_controller.rb
@@ -2,12 +2,10 @@ require_dependency Rails.root.join('app', 'controllers', 'admin', 'poll', 'polls
 
 class Admin::Poll::PollsController
   private
-
     def poll_params
-      params.require(:poll).permit(:name, :starts_at, :starts_at_hour, :ends_at, :ends_at_hour, :geozone_restricted,
-                                  :summary, :description, :results_enabled, :stats_enabled,
-                                  geozone_ids: [],
-                                  image_attributes: [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy])
+      attributes = [:name, :starts_at, :starts_at_hour, :ends_at, :ends_at_hour, :geozone_restricted, :summary, :description, :results_enabled,
+                    :stats_enabled, geozone_ids: [],
+                    image_attributes: image_attributes]
+      params.require(:poll).permit(*attributes, translation_params(Poll))
     end
-
 end
