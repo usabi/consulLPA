32cc3a633942d71e96d56fe9689d1d71bf29c7be
Ignacio Aliende García <ialiendeg@gmail.com>
2020-05-28 09:44:16 +0100

    Budget evaluation changes

--- app/models/custom/budget/investment.rb
+++ app/models/custom/budget/investment.rb
@@ -2,6 +2,9 @@ require_dependency Rails.root.join('app', 'models', 'budget', 'investment').to_s
 class Budget::Investment
   validates :author_phone, presence: true, format: { with: /\A[\d \+]+\z/ }
 
+  scope :takecharged, -> { where(feasibility: "takecharge") }
+  scope :included_next_year_budget, -> { where(feasibility: "nextyearbudget") }
+
   def enough_money?(ballot)
     available_money = ballot.amount_available(heading)
     price <= available_money
@@ -15,4 +18,15 @@ class Budget::Investment
     return :different_heading_assigned unless ballot.valid_heading?(heading)
     return :not_enough_money_html      if ballot.present? && !enough_money?(ballot)
   end
+
+    def self.advanced_filters(params, results)
+      ids = []
+      ids += results.valuation_finished_feasible.pluck(:id) if params[:advanced_filters].include?('feasible')
+      ids += results.where(selected: true).pluck(:id)       if params[:advanced_filters].include?('selected')
+      ids += results.undecided.pluck(:id)                   if params[:advanced_filters].include?('undecided')
+      ids += results.unfeasible.pluck(:id)                  if params[:advanced_filters].include?('unfeasible')
+      ids += results.takecharged.pluck(:id)                 if params[:advanced_filters].include?('takecharged')
+      ids += results.included_next_year_budget.pluck(:id)   if params[:advanced_filters].include?('included_next_year_budget')
+      results.where("budget_investments.id IN (?)", ids)
+    end
 end
