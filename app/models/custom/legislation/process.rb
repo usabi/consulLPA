b19e2a1c0ab912301a3ef991f7f4732808cf305f
Alejandro Vera <alejandro.vera1988@gmail.com>
2021-03-10 16:03:24 +0000

    Comentarios en el código

--- app/models/custom/legislation/process.rb
+++ app/models/custom/legislation/process.rb
@@ -1,6 +1,16 @@
 require_dependency Rails.root.join('app', 'models', 'legislation', 'process').to_s
 class Legislation::Process
-  # documentable max_documents_allowed: 3, TODO: por settings
-  # max_file_size: 5.megabytes,
-  # accepted_content_types: [ "application/pdf" ]
+
+  def self.max_documents_allowed
+    3
+  end
+
+  def self.max_file_size
+    5.megabytes
+  end
+
+  def self.accepted_content_types
+    ["application/pdf"]
+  end
+
 end
