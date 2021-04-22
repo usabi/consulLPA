68faa01cb7f53992f759f6ee85dcbf1cd233fe80
Ignacio Aliende García <ialiendeg@gmail.com>
2021-04-22 17:57:43 +0100

    Fixes uploading in budgets investments management

--- app/controllers/custom/management/budgets/investments_controller.rb
+++ app/controllers/custom/management/budgets/investments_controller.rb
@@ -4,7 +4,22 @@ class Management::Budgets::InvestmentsController
   private
 
     def investment_params
-      attributes = [:external_url, :heading_id, :tag_list, :organization_name, :location, :skip_map, :author_phone]
+      attributes = [:external_url, :heading_id, :tag_list, :organization_name, :location, :skip_map, :author_phone,
+                    image_attributes: image_attributes,
+                    documents_attributes: document_attributes,
+                    map_location_attributes: map_location_attributes]
       params.require(:budget_investment).permit(attributes, translation_params(Budget::Investment))
+    end
+
+    def document_attributes
+      [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy]
+    end
+
+    def image_attributes
+      [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy]
+    end
+
+    def map_location_attributes
+      [:latitude, :longitude, :zoom]
     end
 end
